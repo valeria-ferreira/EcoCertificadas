3a96dbb2d9429c0e8826e959a22247070f022369
valeria-ferreira <ferreira_val@outlook.com>
2023-11-12 01:57:40 -0300

    Arrumando pastas, adicionando Materializecss e dando os primeiros passos com CSS e JavaScript

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// Index Background
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('videoBackground');
+    var img = document.getElementById('imgBackground');
+
+    video.src = './assets/videos/backgroundV.mp4';
+    img.src = './assets/img/imgBackground.jpg';
+});
